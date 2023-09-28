e2b6b0a68c146074461efbf6f69556518e0fb612
Romu10 <romulobryanp@gmail.com>
2023-09-28 01:09:02 +0000

    Robot Working Good

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -20,7 +20,7 @@ class Patrol : public rclcpp::Node {
 
     public:
         // Constructor
-        Patrol() : Node("Node_name"){
+        Patrol() : Node("Robot_Patrol"){
 
         // Laser Sub
         laser_callback_group_ = this->create_callback_group(
@@ -30,13 +30,13 @@ class Patrol : public rclcpp::Node {
         options1.callback_group = laser_callback_group_;
 
         subscription1_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-        odom_topic_name1, 10,
+        "/scan", 10,
         std::bind(&Patrol::Laser_callback, this, std::placeholders::_1),
         options1);
 
         // Cmd_Vel Pub
         publisher1_ = this->create_publisher<geometry_msgs::msg::Twist>(
-            "/cmd_vel", 1);
+        "/cmd_vel", 1);
 
         // Timer Loop 10Hz
         timer_callback_group_ = this->create_callback_group(
@@ -53,33 +53,49 @@ class Patrol : public rclcpp::Node {
 
         // Laser Sub
         void Laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+            // Message received
+            int range_total = msg->ranges.size();
+            RCLCPP_INFO(this->get_logger(), "Total Laser Received: %i", range_total);
+
             // Define constants for the desired angle range (between -π/2 and π/2).
             const float min_angle = -M_PI / 2.0;
             const float max_angle = M_PI / 2.0;
 
-            // Initialize variables to keep track of the maximum range and its corresponding angle.
-            float max_range = 0.0; // Initialize to 0.
-            float max_range_angle = 0.0; // Initialize to 0.
+            // Initialize variables to keep track of the minimum range and its corresponding angle.
+            float min_range = std::numeric_limits<float>::max(); // Initialize to a large value.
+            float min_range_angle = 0.0;
 
             // Iterate through the laser scan data within the desired angle range.
             for (size_t i = 0; i < msg->ranges.size(); ++i) {
                 float angle = msg->angle_min + i * msg->angle_increment;
-                
+
                 // Check if the angle falls within the desired range.
                 if (angle >= min_angle && angle <= max_angle) {
                     float range = msg->ranges[i];
-                    
-                    // Check if the range is finite (not inf) and greater than the current maximum range.
-                    if (std::isfinite(range) && range > max_range) {
-                        max_range = range;
-                        max_range_angle = angle;
+
+                    // Check if the range is finite (not inf), less than the current minimum range, and within the specified threshold.
+                    if (std::isfinite(range) && range < min_range && range <= 0.5) {
+                        min_range = range;
+                        min_range_angle = angle;
                     }
                 }
             }
 
-            // Store the angle in the class variable direction_.
-            direction_ = max_range_angle;
+            // Check if a close obstacle was detected (within 0.5 meters).
+            if (min_range <= 0.3) {
+                // Calculate the opposite direction to move away from obstacles.
+                opposite_direction_ = min_range_angle + M_PI;
+                direction_ = opposite_direction_;
+            }
+            else{
+                opposite_direction_ = 0.00;
+                direction_ = opposite_direction_;
+            }
+                
+
+            RCLCPP_INFO(this->get_logger(), "Direction: %f", direction_);
         }
+
         
         void timer_callback() {
             RCLCPP_INFO(this->get_logger(), "ROBOT CONTROLLER");
@@ -93,9 +109,12 @@ class Patrol : public rclcpp::Node {
             // Define proper angular velocity in Z
             message.angular.z = (direction_/2);
 
+            //Publish the data
+            this->publisher1_->publish(message);
+
             // Print the data 
             RCLCPP_INFO(this->get_logger(), "Linear Velocity in X: %f", message.linear.x);
-            RCLCPP_INFO(this->get_logger(), "Angular Velocity in Y: %f", message.angular.z);
+            RCLCPP_INFO(this->get_logger(), "Angular Velocity in Z: %f", message.angular.z);
 
             return; 
         }
@@ -115,19 +134,19 @@ class Patrol : public rclcpp::Node {
         // Member Variables
         std::list<float> laser_ranges_;
         float direction_;
+        float opposite_direction_ = 0.00;
 };
 
-int main (int argc, char **argv){
+int main (int argc, char *argv[]){
     rclcpp::init(argc, argv);
 
     std::shared_ptr<Patrol> patrol_node = std::make_shared<Patrol>();
-    rclcpp::spin(node);
 
     rclcpp::executors::MultiThreadedExecutor executor;
     executor.add_node(patrol_node);
-    executor.spin(node);
+    executor.spin();
 
     rclcpp::shutdown();
     return 0;
 
-};
+}
